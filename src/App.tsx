b651d5426cd3787323bb6d006a0926423182b1ee
White11010 <beliavski26@gmail.com>
2022-07-30 16:59:16 +0300

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './App.module.scss';
 
+const foo = "bar";
+
 function App() {
-  return (
-    <div className={styles.App}>
-      <p>foooooo</p>
-    </div>
-  );
+	return (
+		<div className={styles.App}>
+			<p>foooooo</p>
+		</div>
+	);
 }
 
 export default App;
