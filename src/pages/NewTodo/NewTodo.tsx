8a82d24459a0ef8429fa75a3e11c42bc98b143eb
White11010 <beliavski26@gmail.com>
2022-07-31 14:57:30 +0300

    add navigation links

--- src/pages/NewTodo/NewTodo.tsx
+++ src/pages/NewTodo/NewTodo.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+
+import styles from './NewTodo.module.scss';
 import TodoEdit from '../../components/core/Todo/TodoEdit/TodoEdit';
+import Button from '../../components/ui/buttons/Button/Button';
 
 const NewTodo = () => {
 	return (
 		<div>
+			<nav className={styles.newTodoNav}>
+				<Button type="button" href="/todos">My todos</Button>
+			</nav>
 			<TodoEdit/>
 		</div>
 	);
